876a8eb524a3e337832e189fb77ba98c80856055
Antonio Jesús Cano Andreu <aca062@inlumine.ual.es>
2022-03-07 14:48:59 +0100

    Primera vista

---
+++ proyecto/frontend/src/vistas/vista-cabecera_pagina.ts
@@ -0,0 +1,43 @@
+import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
+
+@customElement('vista-cabecera_pagina')
+export class VistaCabecera_pagina extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+          display: block;
+          height: 100%;
+      }
+      `;
+  }
+
+  render() {
+    return html`
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct); justify-content: space-between;">
+  <vaadin-button>
+    Button 
+  </vaadin-button>
+  <vaadin-horizontal-layout theme="spacing" style="justify-content: flex-end;" float="" margin-right="2px" margin="">
+   <vaadin-button>
+     Button 
+   </vaadin-button>
+   <vaadin-button>
+     Button 
+   </vaadin-button>
+  </vaadin-horizontal-layout>
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout class="content" style="width: 100%; flex-grow: 1; flex-shrink: 1; flex-basis: auto;"></vaadin-vertical-layout>
+ <vaadin-horizontal-layout class="footer" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct);"></vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
+  }
+
+  // Remove this method to render the contents of this view inside Shadow DOM
+  createRenderRoot() {
+    return this;
+  }
+}
