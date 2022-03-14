ac8676cc3ef60f7a9ca5564772a526fb620d5177
Antonio Jesús Cano Andreu <aca062@inlumine.ual.es>
2022-03-14 14:56:46 +0100

    Vistas y cosas

--- proyecto/frontend/src/vistas/vista-cabecera_pagina.ts
+++ proyecto/frontend/src/vistas/vista-cabecera_pagina.ts
@@ -1,7 +1,9 @@
 import { LitElement, html, css, customElement } from 'lit-element';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
-import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 
 @customElement('vista-cabecera_pagina')
 export class VistaCabecera_pagina extends LitElement {
@@ -16,22 +18,36 @@ export class VistaCabecera_pagina extends LitElement {
 
   render() {
     return html`
-<vaadin-vertical-layout style="width: 100%; height: 100%;">
- <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct); justify-content: space-between;">
-  <vaadin-button>
+<vaadin-vertical-layout style="width: 100%; height: 100%;" id="vaadinVerticalLayout">
+ <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct); justify-content: space-between;" id="vaadinHorizontalLayout">
+  <vaadin-button id="vaadinButton">
     Button 
   </vaadin-button>
-  <vaadin-horizontal-layout theme="spacing" style="justify-content: flex-end;" float="" margin-right="2px" margin="">
-   <vaadin-button>
+  <vaadin-horizontal-layout theme="spacing" style="justify-content: flex-end;" float="" margin-right="200px" id="vaadinHorizontalLayout1" margin="">
+   <vaadin-button id="vaadinButton1">
      Button 
    </vaadin-button>
-   <vaadin-button>
+   <vaadin-button id="vaadinButton2">
      Button 
    </vaadin-button>
   </vaadin-horizontal-layout>
  </vaadin-horizontal-layout>
- <vaadin-vertical-layout class="content" style="width: 100%; flex-grow: 1; flex-shrink: 1; flex-basis: auto;"></vaadin-vertical-layout>
- <vaadin-horizontal-layout class="footer" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct);"></vaadin-horizontal-layout>
+ <vaadin-vertical-layout class="content" style="width: 100%; flex-grow: 1; flex-shrink: 1; flex-basis: auto; justify-content: space-between;" id="vaadinVerticalLayout1">
+  <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout4" style="width: 100%; align-self: center; justify-content: center;">
+   <vaadin-text-field placeholder="Search" id="vaadinTextField" style="width: 50%;">
+    <iron-icon icon="lumo:search" slot="prefix"></iron-icon>
+   </vaadin-text-field>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout id="vaadinHorizontalLayout3" style="width: 100%; align-self: center; align-items: baseline; justify-content: center;background-color: var(--lumo-contrast-10pct);">
+   <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout3" style="width: 20%; justify-content: center; align-items: baseline; flex-grow: 1; flex-shrink: 0;">
+    <h3 id="h3" style="align-self: center;">Contacto</h3>
+   </vaadin-vertical-layout>
+   <vaadin-vertical-layout theme="spacing" id="vaadinVerticalLayout2" style="width: 80%;">
+    <h4 id="h4"> assssssssssssssss</h4>
+   </vaadin-vertical-layout>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-horizontal-layout class="footer" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct);" id="vaadinHorizontalLayout2"></vaadin-horizontal-layout>
 </vaadin-vertical-layout>
 `;
   }
--- proyecto/src/main/java/vistas/VistaCabecera_pagina.java
+++ proyecto/src/main/java/vistas/VistaCabecera_pagina.java
@@ -3,6 +3,13 @@ package vistas;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.textfield.TextField;
 
 /**
  * A Designer generated component for the vista-cabecera_pagina template.
@@ -14,7 +21,38 @@ import com.vaadin.flow.component.littemplate.LitTemplate;
 @JsModule("./src/vistas/vista-cabecera_pagina.ts")
 public class VistaCabecera_pagina extends LitTemplate {
 
-    /**
+    @Id("vaadinVerticalLayout2")
+	private Element vaadinVerticalLayout2;
+	@Id("h3")
+	private H3 h3;
+	@Id("vaadinVerticalLayout3")
+	private Element vaadinVerticalLayout3;
+	@Id("vaadinHorizontalLayout3")
+	private HorizontalLayout vaadinHorizontalLayout3;
+	@Id("vaadinTextField")
+	private TextField vaadinTextField;
+	@Id("h4")
+	private H4 h4;
+	@Id("vaadinHorizontalLayout2")
+	private HorizontalLayout vaadinHorizontalLayout2;
+	@Id("vaadinHorizontalLayout4")
+	private HorizontalLayout vaadinHorizontalLayout4;
+	@Id("vaadinVerticalLayout1")
+	private Element vaadinVerticalLayout1;
+	@Id("vaadinButton2")
+	private Button vaadinButton2;
+	@Id("vaadinButton1")
+	private Button vaadinButton1;
+	@Id("vaadinHorizontalLayout1")
+	private HorizontalLayout vaadinHorizontalLayout1;
+	@Id("vaadinButton")
+	private Button vaadinButton;
+	@Id("vaadinHorizontalLayout")
+	private HorizontalLayout vaadinHorizontalLayout;
+	@Id("vaadinVerticalLayout")
+	private Element vaadinVerticalLayout;
+
+	/**
      * Creates a new VistaCabecera_pagina.
      */
     public VistaCabecera_pagina() {
